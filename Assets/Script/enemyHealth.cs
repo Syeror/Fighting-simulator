7dee867a82cf869ca87e91a21b7efeced8d7af55
Syeror <stavrporo@gmail.com>
2024-03-23 12:58:00 +0300

    3068

--- Assets/Script/enemyHealth.cs
+++ Assets/Script/enemyHealth.cs
@@ -1,23 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class enemyHealth : MonoBehaviour
 {
+    private int Score = 0;
     public float value;
     public PlayerProgress playerProgress;
-   
+    public TextMeshProUGUI CountDestroyed;
+    
+    public void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+
     public void DealDamage(float damage)
     {
-      
-        
+
+
         value -= damage;
         playerProgress.AddExpirence(damage);
-    
+
         if (value <= 0)
         {
             Destroy(gameObject);
+            Score++;
+            UpdateKillCountText();
+
+
         }
     }
-
+    private void UpdateKillCountText()
+    {
+        if (CountDestroyed != null)
+        {
+            CountDestroyed.text =  Score.ToString();
+        }
+    }
 }
